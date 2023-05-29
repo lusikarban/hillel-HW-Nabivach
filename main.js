e235e0d02543d5c0662a3efff7bbcc735a19c60b
Lusi Karban <lusikarban@gmail.com>
2023-05-29 16:27:38 +0300

    add new file main.js,index.html

---
+++ main.js
@@ -0,0 +1,14 @@
+function* getPartsOfWord(string) {
+    let newString = '';
+    let symbol = ''
+
+    for (let i = 0; i < string.length; i++) {
+        newString += string[i]
+        symbol = yield newString + symbol;
+
+        if (symbol === undefined) {
+            symbol = '';
+        };
+    };
+    return newString;
+};
