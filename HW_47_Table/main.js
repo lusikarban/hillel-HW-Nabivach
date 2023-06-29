a45083712f691e96902d223d0df61b2a7dc6a592
Lusi Nabivach <125807694+lusikarban@users.noreply.github.com>
2023-06-29 19:36:25 +0300

    Update main.js

--- HW_47_Table/main.js
+++ HW_47_Table/main.js
@@ -1,18 +1,37 @@
+function createCell(number) {
+    var cell = document.createElement('td');
+    var cellText = document.createTextNode(number);
+    cell.appendChild(cellText);
+    return cell;
+}
 
-var table = document.createElement('table');
+function createRow(start, end) {
+    var row = document.createElement('tr');
+    for (var i = start; i <= end; i++) {
+        var cell = createCell(i);
+        row.appendChild(cell);
+    }
+    return row;
+}
 
-var number = 1;
-for (var i = 0; i < 10; i++) {
-  var row = document.createElement('tr');
-  for (var j = 0; j < 10; j++) {
-    var term = document.createElement('td');
-    term.textContent = number;
-    row.appendChild(term);
-    number++;
-  }
-  table.appendChild(row);
+function createTable(start, end) {
+    var table = document.createElement('table');
+    for (var i = start; i <= end; i += 10) {
+        var row = createRow(i, i + 9);
+        table.appendChild(row);
+    }
+    return table;
+}
+
+function appendTableToContainer(table, containerId) {
+    var container = document.getElementById(containerId);
+    container.appendChild(table);
 }
 
 
-var tableAll = document.getElementById('table'); 
-tableAll.appendChild(table);
+function main() {
+    var table = createTable(1, 100);
+    appendTableToContainer(table, 'table-container');
+}
+
+window.onload = main;
